d8c5c21a4122d6044fedf88a5d464444ba85f06a
mcq4free <142657301+mcq4free@users.noreply.github.com>
2023-09-02 20:27:47 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1> We are online very soon :) </h1>
+      <h1> We are online 1 january 2024 :) </h1>
     </main>
   )
 }
